c1cf7587397252e4b620e2bb3735ba1965f3a42a
florentino1 <1017384222@qq.com>
2021-04-13 23:09:11 +0800

    weather app  first time commit

---
+++ myWhitePonyWeather/myWhitePonyWeather/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  myWhitePonyWeather
+//
+//  Created by 莫玄 on 2021/4/13.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ myWhitePonyWeather/myWhitePonyWeather/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  myWhitePonyWeather
+//
+//  Created by 莫玄 on 2021/4/13.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
